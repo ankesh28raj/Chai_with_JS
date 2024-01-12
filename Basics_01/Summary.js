7f3614ba16a177b077e8892ef34b076f719a2ddf
Ankesh Raj <90593000+ankesh28raj@users.noreply.github.com>
2024-01-12 12:04:45 +0000

    Summary

---
+++ Basics_01/Summary.js
@@ -0,0 +1,29 @@
+//PRIMITIVE(7) => CALL BY VALUE 
+// STRING, NUMBER, BOOLEAN, NULL, UNDEFINED, SYMBOL, BIGINT
+
+// REFERENCE TYPES/NON-PRIMITIVES
+//ARRAYS, OBJECTS, FUNCTIONS
+
+// const score = 100
+// const scoreValue = 100.3
+
+
+// const id = Symbol('123')
+// const anotherId = Symbol("123")
+
+//console.log(id == anotherId);
+
+
+// const heroes = ["Sonpari", "Naagraj", "Doga", "Poer Rangers", "Chellam Sir"]
+// let myObj = {
+//     name : "abc",
+//     age : 22
+// }
+
+const myFuntion = function(){ //treat func as variable
+         console.log("Hello World");
+}
+
+console.log(myFuntion);
+
+

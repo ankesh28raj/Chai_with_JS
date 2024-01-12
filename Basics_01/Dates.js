815b8c8bc93bc5abbd9aa84a48e1cb0e503223f9
Ankesh Raj <90593000+ankesh28raj@users.noreply.github.com>
2024-01-12 14:02:46 +0000

    dates

---
+++ Basics_01/Dates.js
@@ -0,0 +1,22 @@
+
+
+
+let d = new Date()
+// console.log(d);
+// console.log(d.toString());
+// console.log(d.toISOString());
+// console.log(d.toUTCString());
+
+console.log(typeof d);
+
+//let dRandom = new Date("2024-01-28");
+let dRandom = new Date("20-1-2024")
+//console.log(dRandom.toLocaleString());
+
+let myTimeStamp = new Date();
+console.log(myTimeStamp.getTime())
+
+let newDate = new Date();
+console.log(newDate);
+
+console.log(newDate.getMonth());

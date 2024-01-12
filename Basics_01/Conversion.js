3d125722a3be4c13d24741632cd834db80462b87
Ankesh Raj <90593000+ankesh28raj@users.noreply.github.com>
2024-01-12 11:39:56 +0000

    Conversions

--- Basics_01/Conversion.js
+++ Basics_01/Conversion.js
@@ -1,7 +1,7 @@
 let score = true
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 
 let val = Number(score)
@@ -16,10 +16,32 @@ let val = Number(score)
 let state = 1
 
 let booleanState = Boolean(state)
-console.log(booleanState)
+// console.log(booleanState)
 
 let num = 34141
 let stringNum = String(num);
 
-console.log(stringNum);
-console.log(typeof stringNum);
+// console.log(stringNum);
+// console.log(typeof stringNum);
+
+let str1 = "hello "
+let str2 = "buddy"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+2);
+
+// console.log("1"+"2");
+
+// console.log("1"+2+2+3+4);
+
+// console.log(1+2+"2"+3+4);
+
+//console.log(1+2+"2");
+
+//console.log(-true);
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);

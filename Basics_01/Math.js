76ef1f5d49c5ac68736670576ffc86b0d462adeb
Ankesh Raj <90593000+ankesh28raj@users.noreply.github.com>
2024-01-12 13:27:08 +0000

    Math's operations

---
+++ Basics_01/Math.js
@@ -0,0 +1,29 @@
+const score = 400
+
+const balance = new Number(100)
+// console.log(balance);
+// console.log(score);
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2)); // precision to 2 place after decimal
+
+const num = 121.4599
+//console.log(num.toPrecision(3));
+
+const num1 = 1000000000
+//console.log(num1.toLocaleString("en-IN"));
+
+// console.log(Math.abs(-23.23));
+
+// console.log(Math.abs(-4));
+
+
+//------random() method----------
+
+//console.log((Math.random()*10)+1);
+
+const min = 10
+const max = 50
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)

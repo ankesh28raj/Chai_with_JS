a1c6c9e096aaa20c76e0ab25c3a438f1de0aa58a
Ankesh Raj <90593000+ankesh28raj@users.noreply.github.com>
2024-01-12 07:21:45 +0000

    Conversion Of Data Types

---
+++ Basics_01/Conversion.js
@@ -0,0 +1,25 @@
+let score = true
+
+console.log(typeof score);
+console.log(typeof (score));
+
+
+let val = Number(score)
+// console.log(typeof val);
+// console.log(val);
+
+// "33" -> 33
+// "33abc" -> NaN
+//true -> 1; false -> 0
+
+
+let state = 1
+
+let booleanState = Boolean(state)
+console.log(booleanState)
+
+let num = 34141
+let stringNum = String(num);
+
+console.log(stringNum);
+console.log(typeof stringNum);

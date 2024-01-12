dda3fe7068fe1b6c794de3486a2b7a7e71a57c83
Ankesh Raj <90593000+ankesh28raj@users.noreply.github.com>
2024-01-12 06:17:43 +0000

    Data_Types

---
+++ Basics_01/data_types.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all JS code as newer version
+
+//alert("hmm....") //can only be applied in browser
+
+console.log(3+3); 
+console.log("ABC")
+
+let name = "Ankesh"
+let age = 18
+let isLoggedIn = false
+let temp = null
+
+//number, string, boolean, null-> standalone value
+//undefined
+//symbol
+
+console.log(typeof undefined);
+console.log(typeof null);

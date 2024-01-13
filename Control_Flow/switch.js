c9a85aaee4232d7bd0ceab4ead86d2cb6f1a5bb3
Ankesh Raj <90593000+ankesh28raj@users.noreply.github.com>
2024-01-13 06:16:35 +0000

    switch statements

---
+++ Control_Flow/switch.js
@@ -0,0 +1,36 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "Mar"
+
+switch (month) {
+    case "Jan":
+        console.log("Jan");
+        break;
+    case "Feb":
+        console.log("Feb");
+        break;
+    case "Mar":
+        console.log("March");
+        break;
+    case "Apr":
+        console.log("April");
+        break;
+    case "May":
+        console.log("May");
+        break;
+    case "June":
+        console.log("June");
+        break;
+
+    default:
+        console.log("Default case matched!");
+        break;
+}
+

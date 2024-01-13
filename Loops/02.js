f87726c38297548992773ae20e731e9bb59bd108
Ankesh Raj <90593000+ankesh28raj@users.noreply.github.com>
2024-01-13 07:20:31 +0000

    while, do-while

---
+++ Loops/02.js
@@ -0,0 +1,24 @@
+// let index =- 0
+
+// while(index <= 10){
+//     console.log(`Value of index is ${index}`)
+//     index += 2
+// }
+
+let myArr = ['flash', 'superman', 'batman']
+
+let arr = 0
+while(arr < myArr.length){
+    //console.log(`Value is ${myArr[arr]}`);
+    arr++;
+}
+
+
+//----------DO-WHILE----------
+
+let a = 11
+
+do {
+    console.log(`Score is ${a}`);
+    break;
+} while (a <= 10);

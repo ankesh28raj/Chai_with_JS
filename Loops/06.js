54bc663cfc10e205dc4613aebe96f7ac277c710d
Ankesh Raj <90593000+ankesh28raj@users.noreply.github.com>
2024-01-13 12:33:00 +0000

    More on forEach

---
+++ Loops/06.js
@@ -0,0 +1,19 @@
+//const coding = ["js", "rb", "py", "java", "cpp"]
+
+// const values = coding.forEach((item) => {
+//     console.log(item);
+//     //return item
+// })
+
+// console.log(values);
+
+const myNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNum = myNum.filter( (num) =>  {
+//     return num>4 
+// })
+
+//console.log(newNum);
+
+
+

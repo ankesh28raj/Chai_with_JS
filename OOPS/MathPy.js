d2a6bef08693cb8c62dbd2df09a3e16bea74c702
Ankesh Raj <90593000+ankesh28raj@users.noreply.github.com>
2024-01-14 08:01:24 +0000

    Play with Math.PI

---
+++ OOPS/MathPy.js
@@ -0,0 +1,34 @@
+const descriptor = Object.getOwnPropertyDescriptor(Math, "PI")
+
+
+console.log(descriptor);
+// console.log(Math.PI);
+// Math.PI = 5
+// console.log(Math.PI);
+
+
+const chai = {
+    name : 'masala_chai',
+    price : 200,
+    isAvailable : true,
+    orderChai : function(){
+        console.log("Chai nahi bani hai");
+
+    }
+}
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+Object.defineProperty(chai, 'name', {
+    //writable : false,
+    enumerable : true
+})
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+for (const [key, value] of Object.entries(chai)) {
+    if(typeof value != 'function'){
+        console.log(`${key} : ${value}`);
+    }
+    
+    
+}
